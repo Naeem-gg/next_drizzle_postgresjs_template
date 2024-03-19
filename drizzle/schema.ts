842135ecaa7a9100f767f441f61e533f9ab880c5
Naeem Navjivan <n.navjivan@qvitec.in>
2024-03-19 13:03:06 +0530

    schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,5 +1,8 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 //define a simple table
 export const test = pgTable("test",{
-    id:serial("id").primaryKey()
+    id:serial("id").primaryKey(),
+    name:varchar("name").notNull(),
+    password:varchar("password").notNull(),
+    email:varchar("email").notNull()
 })
